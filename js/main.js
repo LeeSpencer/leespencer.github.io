91c69be5ae263761e83c0f58d3a9d19b279e49e8
Spencer Lee <37911329+LeeSpencer@users.noreply.github.com>
2022-02-25 09:44:01 -0500

    implement flipping pages in open book

--- js/main.js
+++ js/main.js
@@ -2,53 +2,114 @@
     "use strict";
 
 
-    // Variable declarations
-    //----------------------
+    //////////////////////////////////////////////
+    // Global variable declarations
+    //////////////////////////////////////////////
     const HTML = {
         noJSElements: document.querySelectorAll('.no-js'),
         jsElements: document.querySelectorAll('.js'),
 
-        bookCover: document.querySelector('.book'),
-        backCover: document.querySelector('.book-backside')
+        book: document.querySelector('.book'),
+        bookCover: document.querySelector('.book-front'),
+        backCover: document.querySelector('.book-backside'),
+        bookPages: document.querySelectorAll('.book-page'),
+        flipPages: document.querySelectorAll(
+                    '.flip-page-1, '+
+                    '.flip-page-2, '+
+                    '.flip-page-3'
+                ),
+        nextButtons: document.querySelectorAll('.next-page-btn'),
+        previousButtons: document.querySelectorAll('.previous-page-btn')
     };
 
-    // Helper Functions
-    //----------------------
+    let pageDeg = -170;
+
+
+
+    //////////////////////////////////////////////
+    // Helper functions
+    //////////////////////////////////////////////
+
+    /** Conveniently set transform, -moz-transform, and
+     * -webkit-transform on the given element. */
+    function setTransform(element, transform) {
+        element.style.transform = transform;
+        element.style.WebkitTransform = transform;
+        element.style.MozTransform = transform;
+    }
+
+    /** Handles the book-flipping-open animation. */
     function openBook() {
-        const bookScale = window.innerHeight/HTML.bookCover.clientHeight;
-        const bookTransform =
-            'translateY(-50%) '+
-            'rotateY(-180deg) '+
-            'scale('+bookScale+')';
 
-        const backTransform = 'translateY(-50%) scale('+bookScale+')';
+        // Calculate how much to scale the book to fill the screen
+        const bookScaleAmount = window.innerHeight/HTML.book.clientHeight;
+        const bookScale = 'scale('+bookScaleAmount+')';
 
-        HTML.bookCover.classList.remove('closed-book');
-        HTML.bookCover.addEventListener('transitionend', ()=>{
+        // Remove closed-book class so that the book "slides" to the right
+        HTML.book.classList.remove('closed-book');
+
+        // Flip the book open when it's done "sliding" to the right
+        HTML.book.addEventListener('transitionend', function animate() {
+            HTML.book.removeEventListener('transitionend', animate);
+
+            for (const page of HTML.bookPages) {
+                page.style.visibility = 'visible';
+            }
             HTML.backCover.style.visibility = 'visible';
-            HTML.backCover.style.transform = backTransform;
-            HTML.backCover.style.WebkitTransform = backTransform;
-            HTML.backCover.style.MozTransform = backTransform;
 
-            HTML.bookCover.classList.add('open-book');
-            HTML.bookCover.style.transform = bookTransform;
-            HTML.bookCover.style.WebkitTransform = bookTransform;
-            HTML.bookCover.style.MozTransform = bookTransform;
+            // (ZOOM, ENHANCE, ENHANCE, ENHANCE)
+            setTransform(HTML.book, 'translateY(-50%) '+bookScale);
+
+            // Flip the front cover open to reveal book contents
+            HTML.book.classList.add('open-book');
+            setTransform(HTML.bookCover, 'rotateY(-180deg)');
+
+            // Flip inside pages animation
+            for (const flipPage of HTML.flipPages) {
+                setTransform(flipPage, 'rotateY('+pageDeg+'deg)');
+                pageDeg += 3;
+            }
+
+            
         });
+    }
+    
+    /** Flips to the next page. */
+    function flipToNextPage(event) {
+        const target = imports.getEventTarget(event);
+        const currentPage = target.closest('.book-page');
+        setTransform(currentPage, 'rotateY('+pageDeg+'deg)');
+        pageDeg += 1;
+    }
+
+    /** Flips to the previous page. */
+    function flipToPreviousPage(event) {
+        const target = imports.getEventTarget(event);
+        const currentPage = target.closest('.book-page');
+        setTransform(currentPage, '');
+        pageDeg -= 1;
+    }
+
+    //////////////////////////////////////////////
+    // Event listeners
+    //////////////////////////////////////////////
+    HTML.book.addEventListener('click', function handleOpen() {
+        openBook();
+        HTML.book.removeEventListener('click', handleOpen);
+    });
+
+    for (const nextBtn of HTML.nextButtons) {
+        nextBtn.addEventListener('click', flipToNextPage);
+    }
+    for (const prevBtn of HTML.previousButtons) {
+        prevBtn.addEventListener('click', flipToPreviousPage);
     }
     
 
 
-    ///////////////////////
-    // Event listeners
-    ///////////////////////
-    HTML.bookCover.addEventListener('click', openBook);
-    
-
-
-    //////////////////////////////////
-    // Mainline logic
-    //////////////////////////////////
+    //////////////////////////////////////////////
+    // Accessibility
+    //////////////////////////////////////////////
 
     // Hide all no-JS elements (because if we're here, obviously
     // JavaScript is enabled)
